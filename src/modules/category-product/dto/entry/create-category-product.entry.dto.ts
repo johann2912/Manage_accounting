44536d47186bfd6c77b119500d774759a46ef6eb
Johann Gomez C <johanngomez02@gmail.com>
2022-12-05 08:51:02 -0500

    [MODULES] changes in dto for create category product

--- src/modules/category-product/dto/entry/create-category-product.entry.dto.ts
+++ src/modules/category-product/dto/entry/create-category-product.entry.dto.ts
@@ -2,10 +2,10 @@ import { ApiProperty } from "@nestjs/swagger";
 import { ICreateCategoryProduct } from "../../interfaces/create-category-product.interface";
 
 export class CategoryProductCrateEntryDto implements ICreateCategoryProduct {
-    @ApiProperty()
-    code?: string;
-    @ApiProperty({ minLength: 2})
-    name?: string;
-    @ApiProperty()
-    description?: string;
+    @ApiProperty({ nullable: false})
+    code: string;
+    @ApiProperty({ minLength: 2, nullable: false})
+    name: string;
+    @ApiProperty({ nullable: false })
+    description: string;
 };
